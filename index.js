68fef566f48b69b930cc16caef68ab5c6b191e0e
cameronehrlich <cameronehrlich@gmail.com>
2015-08-17 02:16:59 -0700

    basic stopping point

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var Parse = require('parse').Parse;
+
+Parse.initialize("YM7X3UG99PpU1V65tceqlp9O4gJk9zXVrpbu4Pdl", "0ND7F8dl430jI6AR822WXjRH8cY1IUNA4lfrwdRZ");
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,7 +13,15 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-	response.render('pages/index');
+
+	var query = new Parse.Query(Parse.User);
+	query.find({
+		success: function(returnedUsers) {
+			response.render('pages/index', {
+				users: returnedUsers
+			});
+		}
+	});
 });
 
 app.listen(app.get('port'), function() {
