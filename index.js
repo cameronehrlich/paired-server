6de9eeec01271095a4601b2f88cf38a2313e6db7
cameronehrlich <cameronehrlich@gmail.com>
2015-08-16 23:40:33 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
@@ -14,19 +13,7 @@ app.get('/', function(request, response) {
 	response.render('pages/index');
 });
 
-app.get('/test', function(request, response) {
-	var result = '';
-	var times = process.env.TIMES || 5;
-	for (var i = 0; i < times; i++) {
-		result += cool();
-	};
-	response.send(result);
-});
-
-app.get('/cool', function(request, response) {
-	response.send(cool());
-});
-
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
 });
+
